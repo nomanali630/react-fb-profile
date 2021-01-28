737b6062735a8957747adee09ea2efe4c4a95a53
nomanali630 <hussainkhaan78@gmail.com>
2021-01-28 16:33:36 +0500

    change

--- src/index.js
+++ src/index.js
@@ -63,7 +63,16 @@ function Mediacard(property) {
     </div>
   </div>
 }
-ReactDOM.render(<Mediacard time={new Date().toLocaleTimeString()} 
+ReactDOM.render(
+<div>
+<Mediacard time={new Date().toLocaleTimeString()} 
 name="House" body="This photo was taken in saadabad palace in Tehran, Iran. we were actually going to take some photos of the buildings, but then we encountered these flowers which were full of butterflies and they were just amazing."
- imageUrl="https://images.all-free-download.com/images/graphicthumb/beautiful_scenery_04_hd_pictures_166258.jpg" />,
+ imageUrl="https://images.all-free-download.com/images/graphicthumb/beautiful_scenery_04_hd_pictures_166258.jpg" />
+ <Mediacard time={new Date().toLocaleTimeString()} 
+name="House" body="This photo was taken in saadabad palace in Tehran, Iran. we were actually going to take some photos of the buildings, but then we encountered these flowers which were full of butterflies and they were just amazing."
+ imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwPbXT3Yw_5OSYDxysni3pYzu0mrkp3A4waw&usqp=CAU" />
+ <Mediacard time={new Date().toLocaleTimeString()} 
+name="House" body="This photo was taken in saadabad palace in Tehran, Iran. we were actually going to take some photos of the buildings, but then we encountered these flowers which were full of butterflies and they were just amazing."
+ imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy-6SKH8f33_wfODM6xzgjZ_5ZP5-4a-PWaA&usqp=CAU" />
+ </div>,
   document.getElementById('root'));
